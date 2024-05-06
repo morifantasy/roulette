14f26b9544894748633ef3e92b71b2068ca3ac6e
mori <mrrsk00guard@gmail.com>
2024-05-07 06:18:22 +0900

    画面遷移機能の追加

--- react-typescript-roulette-app/src/App.tsx
+++ react-typescript-roulette-app/src/App.tsx
@@ -1,15 +1,12 @@
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route} from "react-router-dom"
+import Home from "./mode/Home"
 import Punish_Home from "./mode/punish/Punish_Home"
-import Punish_Add from "./mode/punish/add/Punish_Add"
-import Punish_Roulette from "./mode/punish/roulette/Punish_Roulette"
-
 
 const App = () => {
     return (
         <Routes>
-            <Route path="/" element={<Punish_Home/>} />
-            <Route path="/punish/add" element={<Punish_Add/>} />
-            <Route path="/punish/roulette" element={<Punish_Roulette/>} />
+            <Route path="/" element={<Home/>} />
+            <Route path="/punish" element={<Punish_Home/>} />
         </Routes>
     )
 }
